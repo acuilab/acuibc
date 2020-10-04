c07cd8ce2c116764c1fcb518e4feb9d9c23fcd0b
acuilab <cuizhf@cast514.com>
2020-10-04 14:29:37 +0800

    增加conflux cUSDT代币

---
+++ acuibc-cfx/src/com/acuilab/bc/cfx/CUSDTCoin.java
@@ -0,0 +1,93 @@
+package com.acuilab.bc.cfx;
+
+import conflux.web3j.CfxUnit;
+import java.awt.Image;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.logging.Logger;
+import javax.swing.Icon;
+import org.openide.util.ImageUtilities;
+
+/**
+ *
+ * @author admin
+ */
+public class CUSDTCoin extends ERC20Coin {
+    
+    private static final Logger LOG = Logger.getLogger(CUSDTCoin.class.getName());
+
+    public static final String CONTRACT_ADDRESS = "0x829917146465ded32a7e299328004d30e9c72e80";
+
+    public static final String NAME = "conflux USDT";
+    public static final String SYMBOL = "cUSDT";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getSymbol() {
+        return SYMBOL;
+    }
+
+    @Override
+    public String getBlockChainSymbol() {
+        return CFXBlockChain.SYMBOL;
+    }
+    
+    @Override
+    public Icon getIcon(int size) {
+        return ImageUtilities.loadImageIcon("/resource/cUSDT" + size + ".png", true);
+    }
+
+    @Override
+    public Image getIconImage(int size) {
+        return ImageUtilities.loadImage("/resource/cUSDT" + size + ".png", true);
+    }
+
+    @Override
+    public String getMainUnit() {
+        return "cMOON";
+    }
+
+    @Override
+    public String getMinUnit() {
+        return "";  // 没有最小单位
+    }
+
+    @Override
+    public BigDecimal minUnit2MainUint(BigInteger minUnitValue) {
+        return CfxUnit.drip2Cfx(minUnitValue);
+    }
+
+    @Override
+    public BigInteger mainUint2MinUint(double mainUnitValue) {
+        return CfxUnit.cfx2Drip(mainUnitValue);
+    }
+
+    @Override
+    public BigInteger mainUint2MinUint(long mainUnitValue) {
+        return CfxUnit.cfx2Drip(mainUnitValue);
+    }
+
+    @Override
+    public int getMainUnitScale() {
+        return 6;
+    }
+
+    @Override
+    public int getScale() {
+        return 18;
+    }
+
+    @Override
+    public String getContractAddress() {
+        return CONTRACT_ADDRESS;
+    }
+
+    @Override
+    public boolean isDivisible() {
+        return true;
+    }
+}

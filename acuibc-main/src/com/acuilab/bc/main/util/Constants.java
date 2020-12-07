da5047588e3baae92d7450a76698167c7f9bbc3b
caohongyu <caooooohy@gmail.com>
2020-12-07 13:44:56 +0800

    回复交易日志
    回复交易日志

--- acuibc-main/src/com/acuilab/bc/main/util/Constants.java
+++ acuibc-main/src/com/acuilab/bc/main/util/Constants.java
@@ -7,6 +7,6 @@ package com.acuilab.bc.main.util;
 public class Constants {
     private Constants() {}
     
-    public static final String TITLE = "Gourd —— 社区桌面钱包";
-    public static final String VERSION = "v0.8.4";
+    public static final String TITLE = "宝葫芦Gourd — Conflux社区桌面钱包";
+    public static final String VERSION = "v0.8.6";
 }

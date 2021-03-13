e580d0383493f8724a0482db92d267cba8318c92
acuilab <45508740+acuilab@users.noreply.github.com>
2021-03-13 12:25:57 +0800

    change log

---
+++ acuibc-main/src/com/acuilab/bc/main/action/ChangeLogTopComponentAction.java
@@ -0,0 +1,27 @@
+package com.acuilab.bc.main.action;
+
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import org.openide.windows.TopComponent;
+import org.openide.windows.WindowManager;
+
+/**
+ *
+ * @author admin
+ */
+public class ChangeLogTopComponentAction extends AbstractAction {
+    
+    public ChangeLogTopComponentAction() {
+        putValue(NAME, "CHANGELOG");
+//        putValue(SMALL_ICON, new javax.swing.ImageIcon(getClass().getResource("/resource/wallet16.png")));
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	TopComponent tc = WindowManager.getDefault().findTopComponent("ChangeLogTopComponent");
+	if(tc != null) {
+	    tc.open();
+	    tc.requestActive();
+	}
+    }
+}
---
+++ acuibc-main/src/resource/changelog/CHANGELOG.md
@@ -0,0 +1,19 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Added
+- conflux批量转账刷新交易状态时写入日志窗口
+- 增加CHANGELOG支持
+
+### Fixed
+- conflux代币交易日志发送方和接收方转换为简化形式地址
+
+
+
+[^_^]:http://jbt.github.io/markdown-editor/

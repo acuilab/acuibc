99854adb0b0ae8470e90bd415f982a6b7b80903b
acuilab <78429471@qq.com>
2021-03-24 14:05:54 +0800

    ..

--- acuibc-cfx/src/com/acuilab/bc/cfx/FCExchange.java
+++ acuibc-cfx/src/com/acuilab/bc/cfx/FCExchange.java
@@ -1,10 +1,7 @@
 package com.acuilab.bc.cfx;
 
-import static com.acuilab.bc.cfx.CFXCoin.STAKING_CONTRACT_ADDRESS;
 import com.acuilab.bc.main.coin.IFCExchange;
-import conflux.web3j.Account;
 import conflux.web3j.Cfx;
-import conflux.web3j.types.Address;
 import java.util.logging.Logger;
 import org.openide.util.Lookup;
 
@@ -19,12 +16,11 @@ public class FCExchange implements IFCExchange {
     public static final String CONTRACT_ADDRESS = "cfx:acdrd6ahf4fmdj6rgw4n9k4wdxrzfe6ex6jc7pw50m";
 
     @Override
-    public UserInfo userInfos(String privateKey) {
+    public UserInfo userInfos() {
         CFXBlockChain bc = Lookup.getDefault().lookup(CFXBlockChain.class);
         Cfx cfx = bc.getCfx();
         
-        Account account = Account.create(cfx, privateKey);
-        return account.call(new Address(CONTRACT_ADDRESS), "withdraw", new org.web3j.abi.datatypes.Uint(value));
+        return null;
     }
 
 }

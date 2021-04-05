81426f484523993a98f403990b3ef22bf5d00261
caohongyu <caooooohy@gmail.com>
2021-04-05 23:20:09 +0800

    整理三个新的nft

--- acuibc-cfx/src/com/acuilab/bc/cfx/CottonNFT.java
+++ acuibc-cfx/src/com/acuilab/bc/cfx/CottonNFT.java
@@ -36,6 +36,7 @@ public class CottonNFT extends AbstractNFT {
         BigInteger[] ids = super.tokensOf(address); //To change body of generated methods, choose Tools | Templates.
         List<BigInteger> list = Lists.newArrayList();
         for(BigInteger id : ids) {
+            //棉花的tokenid是从955到1154的200个
             if(id.compareTo(BigInteger.valueOf(954)) > 0 && id.compareTo(BigInteger.valueOf(1154)) <= 0) {
                 list.add(id);
             }
@@ -89,36 +90,33 @@ public class CottonNFT extends AbstractNFT {
         String value = contract.call("uri", new org.web3j.abi.datatypes.Uint(tokenId)).sendAndGet();
         String json = DecodeUtil.decode(value, org.web3j.abi.datatypes.Utf8String.class);
 	
-        System.out.println(json);
+        //System.out.println(json);
 	// 8016{"body":"000","eye":"013","nose":"001","auxiliary":"001","background":"000","clothes":"006","props":"000","title":"002_000_000_013_001_000_001_000","url":"002_000_000_013_001_000_001_000.png"}
 	MetaData md = new MetaData();
 	//md.setName("Activity NFT");
 	md.setPlatform("Tspace");
 	
 	String id = tokenId.toString();
-	md.setId(id);
+	//md.setId(id);
 	
 	ObjectMapper mapper = new ObjectMapper();
 	Map<String, String> map = mapper.readValue(StringUtils.substringAfter(json, id), Map.class);
 	
 	String symbol = map.get("nftSymbol");
         
-                md.setName("棉花NFT");
-		md.setDesc("【中本聪棉花】是由加密艺术家@XIN主创的作品，包含了神秘的比特币创始人中本聪，烤仔的宠物烤喵，顶级POW公链Conflux和它的两种链上通证FC、CFX，新疆长绒棉等一系列元素，由全世界最好的、最顶级的素材构成了整副NFT，含蓄地表达了去中心化世界的民族自信和爱国主义思潮。\n" +
+        md.setName("棉花NFT");
+	md.setDesc("【中本聪棉花】是由加密艺术家@XIN主创的作品，包含了神秘的比特币创始人中本聪，烤仔的宠物烤喵，顶级POW公链Conflux和它的两种链上通证FC、CFX，新疆长绒棉等一系列元素，由全世界最好的、最顶级的素材构成了整副NFT，含蓄地表达了去中心化世界的民族自信和爱国主义思潮。\n" +
 "      本次公益活动，Tspace共铸造200个 【中本聪棉花】NFT，统一售价为20   wCFX（注：wCFX为CFX在Tspace平台映射的代币形式），且永不增发。值得小伙伴们注意的是，为了将加密朋克的爱国主义进行到底，凡是购买该款NFT的用户，都将获得一件同样款式的纪念T恤。\n" +
 "       与此同时，Tspace 将会参与由 CottonDAO 发起的 \"行为艺术即空投\" 的活动，为所有参与这个主题创造的艺术家和参与购买爱国主义NFT的用户申请 Cotton 代币空投奖励，可在日后共同参与治理 Cotton DAO 社区。\n" +
 "       最后，作为这款爱国主义NFT的主创，加密艺术家@XIN宣布此次公益活动的盈利所得将全部捐赠给慈善事业。");
-                md.setImage(ImageUtilities.loadImage("/resource/Cotton178.png", true));
+        md.setImage(ImageUtilities.loadImage("/resource/Cotton178.png", true));
                 
+        //棉花NFT的tokenid和棉花编号对应关系略特殊，155-200号是tokenid955至999，1-154号是1001至1054.
         if(NumberUtils.toInt(id)<1000){
             md.setId("" + (NumberUtils.toInt(id)-955+156));
         }else{
             md.setId("" + (NumberUtils.toInt(id)-1000+1));
         }
-	
-//md.setImageUrl("http://cdn.tspace.online/Cotton/cotton.png");
-	//String title = map.get("title");
-	//md.setImageUrl("http://cdn.tspace.online/image/finish/" + map.get("url"));
 	
 	
 	return md;

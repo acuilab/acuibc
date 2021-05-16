f0d82ea4c17d543a2a31cebaf9e8818414a18b11
acuilab <78429471@qq.com>
2021-05-16 19:17:54 +0800

    ..

---
+++ acuibc-cfx/src/com/acuilab/bc/cfx/solidity/CompilerResult.java
@@ -0,0 +1,22 @@
+package com.acuilab.bc.cfx.solidity;
+
+/**
+ * Container for the compile result.
+ *
+ */
+public class CompilerResult {
+    public String errors;
+    public String output;
+    private boolean success = false;
+
+    public CompilerResult(String errors, String output, boolean success) {
+        this.errors = errors;
+        // https://ethereum.stackexchange.com/questions/11912/unable-to-define-greetercontract-in-the-greeter-tutorial-breaking-change-in-sol
+        this.output = output.replaceAll("<stdin>:", "");
+        this.success = success;
+    }
+
+    public boolean isFailed() {
+        return !success;
+    }
+}

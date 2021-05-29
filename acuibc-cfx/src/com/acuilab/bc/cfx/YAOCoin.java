e553fed56257a4fea4cd791463d133c8a110e4e4
caohongyu <caooooohy@gmail.com>
2021-05-29 16:31:16 +0800

    支持古国的yao
    支持古国的yao

---
+++ acuibc-cfx/src/com/acuilab/bc/cfx/YAOCoin.java
@@ -0,0 +1,94 @@
+package com.acuilab.bc.cfx;
+
+import com.acuilab.bc.main.util.Constants;
+import conflux.web3j.CfxUnit;
+import java.awt.Image;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.logging.Logger;
+import javax.swing.Icon;
+import org.openide.util.ImageUtilities;
+
+/**
+ *
+ * @author admin
+ */
+public class YAOCoin extends ERC20Coin {
+    
+    private static final Logger LOG = Logger.getLogger(YAOCoin.class.getName());
+
+    public static final String CONTRACT_ADDRESS = "cfx:acaucwuza1nm7wfj1bwkjttz7b0eh4ak7ur7fue1dy";
+
+    public static final String NAME = "Anicent Chinese God YAO";
+    public static final String SYMBOL = "YAO";
+
+    @Override
+    public String getName() {
+        return NAME;
+    }
+
+    @Override
+    public String getSymbol() {
+        return SYMBOL;
+    }
+
+    @Override
+    public String getBlockChainSymbol() {
+        return Constants.CFX_BLOCKCHAIN_SYMBAL;
+    }
+    
+    @Override
+    public Icon getIcon(int size) {
+        return ImageUtilities.loadImageIcon("/resource/yao" + size + ".png", true);
+    }
+
+    @Override
+    public Image getIconImage(int size) {
+        return ImageUtilities.loadImage("/resource/yao" + size + ".png", true);
+    }
+
+    @Override
+    public String getMainUnit() {
+        return "YAO";
+    }
+
+    @Override
+    public String getMinUnit() {
+        return "";  // 没有最小单位
+    }
+
+    @Override
+    public BigDecimal minUnit2MainUint(BigInteger minUnitValue) {
+        return CfxUnit.drip2Cfx(minUnitValue);
+    }
+
+    @Override
+    public BigInteger mainUint2MinUint(double mainUnitValue) {
+        return CfxUnit.cfx2Drip(mainUnitValue);
+    }
+
+    @Override
+    public BigInteger mainUint2MinUint(long mainUnitValue) {
+        return CfxUnit.cfx2Drip(mainUnitValue);
+    }
+
+    @Override
+    public int getMainUnitScale() {
+        return 6;
+    }
+
+    @Override
+    public int getScale() {
+        return 18;
+    }
+
+    @Override
+    public String getContractAddress() {
+        return CONTRACT_ADDRESS;
+    }
+
+    @Override
+    public boolean isDivisible() {
+        return true;
+    }
+}
